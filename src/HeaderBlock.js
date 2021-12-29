0fc7062c81905b9f9bce40500e36ea2c58a81012
Zelenskih Mark <z.mark366@gmail.com>
2021-12-29 17:13:14 +0700

    Add files via upload

--- src/HeaderBlock.js
+++ src/HeaderBlock.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 import { UserContext } from './contexts/UserContext'
 
 import './HeaderBlock.css'
-
+import logo from "./UI/logo.svg";
 
 export default class HeaderBlock extends Component {
 
@@ -14,15 +14,14 @@ export default class HeaderBlock extends Component {
     return (
       <header className={'header'}>
         <div className={'header-logo'}>
-          <Link to={'/'}><img src="logo.svg" alt="SAS"/></Link>
-        </div>
+          <Link to={'/'}><img src={logo} width={60} height={60}/></Link>
+        </div >
 
         <div className={'header-main-refs'}>
           <Link to={'/'}>Главная</Link>
           <Link to={'/documentation'}>Документация</Link>
           <Link to={'/sensors'}>Датчики</Link>
         </div>
-
 
         {!isAuthorized &&
           <div className={'header-sign-refs'}>
@@ -37,6 +36,30 @@ export default class HeaderBlock extends Component {
             <Link to={'/signOut'}>Выйти</Link>
           </div>
         }
+
+        <div className="burger-menu">
+            <input id="menu_toggle" type="checkbox"/>
+            <label className="menu_btn" htmlFor="menu_toggle">
+                <span></span>
+            </label>
+            <ul className="menu_box">
+                <li><Link to={'/'}><a className="menu_item">Главная</a></Link></li>
+                <li><Link to={'/documentation'}><a className="menu_item">Документация</a></Link></li>
+                <li><Link to={'/sensors'}><a className="menu_item">Датчики</a></Link></li>
+                {!isAuthorized &&
+                <div className={'header-sign-refs-menu'}>
+                    <li><Link to={'/signUp'}><a className="menu_item">Зарегистрироваться</a></Link></li>
+                    <li><Link to={'/signIn'}><a className="menu_item">Войти</a></Link></li>
+                </div>
+                }
+                {isAuthorized &&
+                <div className={'header-sign-refs-menu'}>
+                    <li><Link to={'/account'}><a className="menu_item">{login}</a></Link></li>
+                    <li><Link to={'/signOut'}><a className="menu_item">Выйти</a></Link></li>
+                </div>
+                }
+            </ul>
+        </div>
       </header>
     )
   }
